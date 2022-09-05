9ca035309b62e915ef33ebf584cfdd614c63de7b
Robert Eads <robert.f.eads@tamu.edu>
2022-09-05 18:07:05 -0500

    Styled service and footer section

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Side Navbar
 function navOpen() {
     document.getElementById("navbar").style.width = "10rem";
 }
@@ -5,3 +6,14 @@ function navOpen() {
 function navClose() {
     document.getElementById("navbar").style.width = "0";
 }
+
+
+//Date for copyright
+function getDate() {
+    const new_date = new Date();
+    let year = new_date.getFullYear();
+    document.getElementById("current").innerHTML = year;    
+}
+
+
+//Style Switching
