f8ac0dc4f0f20a9bbc7674d7c6ce212278a5978f
Robert Eads <robert.f.eads@tamu.edu>
2022-09-12 16:26:55 -0500

    Finished technical sections style

--- scripts.js
+++ scripts.js
@@ -9,22 +9,14 @@ function navClose() {
 
 //Style 2 - Slide open tabs
 function openPage(title) {
-    let section_width = document.getElementById(title).style.width;
-    if(section_width == 0 || section_width == "0vw") {
-        document.getElementById(title).style.width = "92vw";
-        document.getElementById(title + "-button").classList.add("open");
-    }
-    else{
-        document.getElementById(title).style.width = "0vw";
-        document.getElementById(title + "-button").classList.remove("open");
-        document.getElementById(title + "-button").classList.add("close");
-        setTimeout(() => {
-            document.getElementById(title + "-button").classList.remove("close");
-        }, 1000);      
-    }
-
+    document.getElementById(title).classList.toggle("sliding-page");
+    document.getElementById(title+"-button").classList.toggle("open");
 }
 
+//Rotate card elements
+function rotateCard(tag) {
+    document.getElementById(tag).classList.toggle("card-rotate");
+}
 
 //Date for copyright
 function getDate() {
@@ -39,6 +31,7 @@ function styleSwitch() {
     //Choosing opposite style sheet
     let curr_num = document.getElementById("styling").getAttribute("href").substring(6,7) 
     curr_num == 1 ? new_style = "style-2.css" : new_style = "style-1.css";
+
     //Updating and saving style sheet
     document.getElementById("styling").setAttribute("href", new_style);
     fixWidth(curr_num);
@@ -55,16 +48,9 @@ window.onload = function(){
 
 //Fixing div widths after style change
 function fixWidth(num) {
-    if(num == 1) {
-        document.getElementById("tech").style.width = "0";
-        document.getElementById("qual").style.width = "0";
-        document.getElementById("serv").style.width = "0";
-    }
-    else {
-        document.getElementById("tech").style.width = "100vw";
-        document.getElementById("qual").style.width = "100vw";
-        document.getElementById("serv").style.width = "100vw";
-        document.getElementById("navbar").style.width = "0";
-    }
+    document.getElementById("tech").classList.toggle("style1-width");
+    document.getElementById("qual").classList.toggle("style1-width");
+    document.getElementById("serv").classList.toggle("style1-width");
+    if(num == 1) {document.getElementById("navbar").style.width = "0";}
 }
 
